cad7f6f549c258a3faf1845c8a345ada1b640a14
kibuchi muchai <kibuchimuchai@gmail.com>
2022-02-06 17:08:26 +0300

    create a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesAppComponent } from './quotes-app/quotes-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesAppComponent
   ],
   imports: [
     BrowserModule,
